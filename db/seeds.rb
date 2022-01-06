b910fc9dced56d54291f27bcf4228f6e55c43f06
nicolas jambon <nicolas_jambon@hotmail.fr>
2022-01-06 19:35:03 +0100

    challegne 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,20 @@ puts "Cleaning up database..."
 Movie.destroy_all
 puts "Database cleaned"
 
-url = "http://tmdb.lewagon.com/movie/top_rated"
-1000.times do |i|
-  puts "Importing movies from page #{i + 1}"
-  movies = JSON.parse(open("#{url}?page=#{i + 1}").read)['results']
-  movies.each do |movie|
-    puts "Creating #{movie['title']}"
-    base_poster_url = "https://image.tmdb.org/t/p/original"
-    Movie.create(
-      title: movie['title'],
-      overview: movie['overview'],
-      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
-      rating: movie['vote_average']
-    )
+for i in 1..100
+  url = "http://tmdb.lewagon.com/movie/top_rated?&page=#{i}"
+  URI.open(url) do |stream|
+    films = JSON.parse(stream.read)["results"]
+    films.each do |hash|
+      movie = Movie.create(
+        title: hash["original_title"],
+        overview: hash['overview'],
+        poster_url: "https://image.tmdb.org/t/p/original#{hash['backdrop_path']}",
+        rating: hash['vote_average'])
+
+      puts "Created #{movie.title}"
+      end
+    end
   end
-end
+
 puts "Movies created"
